fd34bbedafeece4bd07c2d153bd579c8e9d5aa35
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2022-05-16 01:15:35 +0300

    wip

--- packages/graphql/src/search/unions/search-result.union.ts
+++ packages/graphql/src/search/unions/search-result.union.ts
@@ -4,10 +4,59 @@ import { Continent } from '../../continents/models/continent.model';
 import { State } from '../../states/models/state.model';
 import { Sprite } from '../../sprites/models/sprite.model';
 import { Resource } from '../../resources/model/resource.model';
+import { Ability } from '../../abilities/models/ability.model';
+import { AutonomyState } from '../../autonomous-states/models/autonomy-state.model';
+import { Bookmark } from '../../bookmarks/models/bookmark.model';
+import { Building } from '../../buildings/models/building.model';
+import { Character } from '../../characters/models/character.model';
+import { Decision } from '../../decisions/models/decision.model';
+import { DifficultySetting } from '../../difficulty-settings/models/difficulty-setting.model';
+import { Event } from '../../events/event.model';
+import { FocusTree } from '../../focus-trees/models/focus-tree.model';
+import { Focus } from '../../focuses/models/focus.model';
+import { GameRule } from '../../game-rules/models/game-rule.model';
+import { Idea } from '../../ideas/models/idea.model';
+import { Ideology } from '../../ideologies/models/ideology.model';
+import { IntelligenceAgency } from '../../intelligence-agencies/models/intelligence-agency.model';
+import { Province } from '../../provinces/models/province.model';
+import { StateCategory } from '../../state-categories/models/state-category.model';
+import { StrategicRegion } from '../../strategic-regions/models/strategic-region.model';
+import { Technology } from '../../technologies/models/technology.model';
+import { TerrainCategory } from '../../terrain-categories/models/terrain-category.model';
+import { Unit } from '../../units/models/unit.model';
+import { WarGoal } from '../../war-goals/models/war-goal.model';
 
 export const SearchResultUnion = createUnionType({
   name: 'SearchResultUnion',
-  types: () => [Country, Continent, Resource, Sprite, State] as const,
+  types: () =>
+    [
+      Ability,
+      AutonomyState,
+      Bookmark,
+      Building,
+      Character,
+      Continent,
+      Country,
+      Decision,
+      DifficultySetting,
+      Event,
+      FocusTree,
+      Focus,
+      GameRule,
+      Idea,
+      Ideology,
+      IntelligenceAgency,
+      Province,
+      Resource,
+      Sprite,
+      StateCategory,
+      State,
+      StrategicRegion,
+      Technology,
+      TerrainCategory,
+      Unit,
+      WarGoal,
+    ] as const,
   resolveType(value) {
     return value.constructor.name;
   },
--- packages/web/components/Search.tsx
+++ packages/web/components/Search.tsx
@@ -1,13 +1,63 @@
 import React from 'react';
-import { InputGroup } from '@blueprintjs/core';
+import { InputGroup, MenuItem } from '@blueprintjs/core';
+import { useFormik } from 'formik';
+import { useLazyQuery } from '@apollo/client';
+import SearchQuery from '../graphql/queries/search/search-query.graphql';
+import { Suggest } from '@blueprintjs/select';
+
+const SearchSuggest = Suggest.ofType<any>();
 
 const Search: React.FC = () => {
+  const [search, { data, loading }] = useLazyQuery(SearchQuery, {
+    context: {
+      headers: {
+        'X-Product-Name': 'kaiserreich',
+        'X-Product-Version': '0.20.1',
+      },
+    },
+  });
+  const { setFieldValue, values, handleSubmit } = useFormik({
+    initialValues: {
+      term: '',
+    },
+    async onSubmit(values) {
+      await search({ variables: { term: values.term } });
+    },
+  });
+  React.useEffect(() => {
+    console.log('values', values);
+    handleSubmit();
+  }, [handleSubmit, values]);
+  console.log('data', data);
   return (
-    <InputGroup
-      placeholder="Search..."
-      leftIcon="search"
-      style={{ minWidth: 300 }}
-    />
+    <>
+      <SearchSuggest
+        onQueryChange={(query) => setFieldValue('term', query)}
+        inputValueRenderer={() => ''}
+        onItemSelect={() => {}}
+        items={data?.search ?? []}
+        popoverProps={{
+          position: 'bottom',
+        }}
+        itemRenderer={(result) => {
+          console.log('result', result);
+          switch (result.__typename) {
+            case 'Focus': {
+              return <MenuItem text={result.name} />;
+            }
+          }
+          return <MenuItem text="erewr" />;
+        }}
+      />
+      {/*<InputGroup*/}
+      {/*  //disabled={loading}*/}
+      {/*  name="term"*/}
+      {/*  placeholder="Search..."*/}
+      {/*  leftIcon="search"*/}
+      {/*  style={{ minWidth: 300 }}*/}
+      {/*  onChange={handleChange}*/}
+      {/*/>*/}
+    </>
   );
 };
 
